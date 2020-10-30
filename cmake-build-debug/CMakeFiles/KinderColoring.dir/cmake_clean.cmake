a0250d21549e54fd72df7fd40aac5e8c86515957
karoly15 <karolyi15@hotmail.com>
2020-10-30 02:36:28 -0600

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/KinderColoring.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KinderColoring.dir/main.cpp.o"
+  "KinderColoring"
+  "KinderColoring.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KinderColoring.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/KinderColoring.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for KinderColoring.
+# This may be replaced when dependencies are built.
