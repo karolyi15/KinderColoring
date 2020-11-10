23c0c103ffc573e61e8b2e02e77b843a88867f35
AM4111 <pauma1018@gmail.com>
2020-11-09 18:00:49 -0600

    Limpiado
    .

--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
-#include <fstream>
 #include <rapidxml_utils.hpp>
 #include "rapidxml.hpp"
-#include "Headers/XmlManager.h"
 #include "Headers/Node.h"
+#include "Headers/XmlManager.h"
 #include "Headers/LinkedList.h"
 #include <vector>
+
 using namespace rapidxml;
 using namespace std;
-
 
 // Se envia un string grande y
 // Retorna un vector con todas las separaciones de el, donde este el delim
@@ -28,7 +27,6 @@ vector<string> split(const string& str, const string& delim)
     while (pos < str.length() && prev < str.length());
     return tokens;
 }
-
 
 vector<string> changePoints(string points,vector<string> contry){
     float X = 0;
@@ -51,15 +49,14 @@ vector<string> changePoints(string points,vector<string> contry){
 }
 
 
-
 int main() {
 
     // se crea un vector de vectores de string matriz
-
     vector<vector<string>> matriz;
+
+
     //*****************************************************************************************************************//
     //***** XML MANAGER TEST *****//
-
     //Init Xml Manager
     XmlManager *manager = XmlManager::getInstance();
 
@@ -74,59 +71,23 @@ int main() {
     //recore todos los path
     for (; path != NULL; path = path->next_sibling()) {
 
-
         //crea un vector contry que vamos a meter a la matriz
         vector<string> contry = {};
-        //Point tiene todas lo que este en d
-        // Es decir m y los puntos hasta z
-        //A contry se le va metiendoel primer valor
-        //Es decir el id ejemplo    AF
+
+        //Point tiene todas lo que este en d -> Es decir m y los puntos hasta z
+        //A contry se le va metiendo el primer valor -> Es decir el id ejemplo    AF
         contry.push_back(path->first_attribute()->next_attribute()->value());
         float X=0;
         float Y=0;
         contry=changePoints(path->first_attribute()->next_attribute()->next_attribute()->next_attribute()->next_attribute()->value(),contry);
         matriz.push_back(contry);
     }
-        for (int i = 0; i < matriz.size(); i++) {
-            cout << matriz.at(i).at(0) << endl;
-            cout<< matriz.at(i).at(1)<<endl;
-        }
-        //
-        //Save Xml to File
-        //manager->writeXML("/home/gunther/CLionProjects/KinderColoring/_MapFiles/test.svg");
 
-        //*****************************************************************************************************************//
+    //Verificar Matriz
+    for (int i = 0; i < matriz.size(); i++) {
+        cout << matriz.at(i).at(0) << endl;
+        cout<< matriz.at(i).at(1)<<endl;
+    }
 
-        //*****************************************************************************************************************//
-        //***** NODE TEST *****//
-        /*int value = 1;
-        Node<int> *node = new Node<int>(3);
-        Node<int> *node2 = new Node<int>(4);
-
-        node->setNext(node2);
-
-        cout << "Node value: " << node->getValue() << endl;
-        cout << "Node previous: " << node->getPrevious() << endl;
-        cout << "Node next: " << node->getNext()->getValue() << endl;
-        */
-        //*****************************************************************************************************************//
-
-        //*****************************************************************************************************************//
-        //***** LIST TEST *****/
-        /*
-         LinkedList<string> *list = new LinkedList<int>();
-         list->add(3);
-         list->add(4);
-         list->add(5);
-         list->add(6);
-         list->add(7);
-         //list->print();
-     */
-        //cout << endl;
-        //cout<< "List Get value Test: " << list->getNode(1)->getValue();
-        //*****************************************************************************************************************//
-        return 0;
-
-
-
+    return 0;
 }
